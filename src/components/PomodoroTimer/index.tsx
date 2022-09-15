ce060f0ac2463aa5f04fb454cc214aa94247c0b4
Guilherme Ribeiro <xxguilhermesoaresxx@gmail.com>
2022-09-15 20:43:23 -0300

    commit antes de tentar refatorar com useReducer

--- src/components/PomodoroTimer/index.tsx
+++ src/components/PomodoroTimer/index.tsx
@@ -1,46 +1,51 @@
+import { getMinutes, getSeconds } from "../../utils";
+import { useReducer, useState } from "react";
+
 import {
   buildStyles,
   CircularProgressbarWithChildren,
 } from "react-circular-progressbar";
 
-import { useState } from "react";
-import { getMinutes, getSeconds } from "../../utils";
+type resetSessionType = (interval : NodeJS.Timer | undefined, timeout : NodeJS.Timer | undefined) => void
 
 export const PomodoroTimer = () => {
 
   const [seconds, setSeconds] = useState(10);
   const [playButton, setPlayButton] = useState("START");
-	const [progress, setProgress] = useState(0);
-	const [intervalId, setIntervalId] = useState<NodeJS.Timer>()
-	const [timeoutId, setTimeoutId] = useState<NodeJS.Timer>()
+  const [progress, setProgress] = useState(0);
+  const [intervalId, setIntervalId] = useState<NodeJS.Timer>();
+  const [timeoutId, setTimeoutId] = useState<NodeJS.Timer>();
+
 
   const playSession = () => {
 
-    if(playButton == "START") {
+    if (playButton == "START") {
+      setPlayButton("PAUSE");
 
-			setPlayButton("PAUSE");
-
-			const interval = setInterval(() => { 
-				setSeconds(prevState => prevState - 1);
-				setProgress(prevState => prevState + 1);
-			}, 1000);
+      const interval = setInterval(() => {
+        setSeconds(prevState => prevState - 1);
+        setProgress( prevState => prevState + 1)}, 1000
+			);
 			setIntervalId(interval);
 
-			const timeout = setTimeout(() => {
-				clearInterval(interval);
-				setPlayButton("START");
-				setSeconds(10);
-				setProgress(0);
-			}, seconds * 1000);
-			setTimeoutId(timeout);
+      const timeout : NodeJS.Timer = setTimeout(() => resetSession(interval, timeout), seconds * 1000);
+      setTimeoutId(timeout);
 
-    }	else {
+    } else {
       clearInterval(intervalId);
-			clearTimeout(timeoutId);
+      clearTimeout(timeoutId);
       setPlayButton("START");
     }
-		
+
   };
+
+	const resetSession : resetSessionType = (interval, timeout) => {
+		clearInterval(interval);
+		clearTimeout(timeout);
+		setSeconds(10);
+    setProgress(0);
+		setPlayButton("START");
+	}
 
   const minutesRemaining = getMinutes(seconds);
   const secondsRemaining = getSeconds(seconds);
@@ -49,13 +54,18 @@ export const PomodoroTimer = () => {
     <div>
       <CircularProgressbarWithChildren
         value={progress}
-				minValue={0}
-				maxValue={10}
-        styles={buildStyles({ pathColor: "blue", trailColor: 'red'})}
+        minValue={0}
+        maxValue={10}
+        styles={buildStyles({ pathColor: "blue", trailColor: "red" })}
       >
-				<h1>{minutesRemaining}:{secondsRemaining}</h1>
+        <h1>
+          {minutesRemaining}:{secondsRemaining}
+        </h1>
       </CircularProgressbarWithChildren>
-      <button onClick={playSession}>{playButton}</button>
+      <div>
+				<button onClick={playSession}>{playButton}</button>
+				<button onClick={() => resetSession(intervalId, timeoutId)}>RESET</button>
+			</div>
     </div>
   );
 };
